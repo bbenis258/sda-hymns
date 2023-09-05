c174fbdd66fe24c3d74d34bd3b7475dc7f0fbf87
Benis Abimana <b.abimana@irembo.com>
2023-09-05 21:00:21 +0200

    Implemented hymn search

--- src/main/java/rw/sda/sdahymns/hymn/repo/HymnRepo.java
+++ src/main/java/rw/sda/sdahymns/hymn/repo/HymnRepo.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface HymnRepo extends JpaRepository<Hymn, Long> {
 
-    @Query("SELECT h FROM Hymn h WHERE lower(h.title) LIKE %?1%")
+    @Query("SELECT h FROM Hymn h WHERE lower(h.title) LIKE %?1% OR CAST(h.id AS string) LIKE %?1%")
     List<Hymn> searchHymn(String searchTerm);
 }
